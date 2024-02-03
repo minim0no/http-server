cf5532b13685577537dae23ce40393a8e9f3acc9
minim0no <ahmed.idrees3110@gmail.com>
2024-02-03 00:08:30 -0600

    bind socket

--- http_tcpServer.cpp
+++ http_tcpServer.cpp
@@ -1,35 +1,58 @@
 #include <http_tcpServer.hpp>
 #include <stdexcept>
 
-class TcpServer {
+class TcpServer
+{
     // constructors/destructors
-    TcpServer::TcpServer() {}
-    TcpServer::~TcpServer(){
-        closeServer();
+    TcpServer::TcpServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port)
+    {
+        m_socketAddress.sin_family = AF_INET;
+        m_socketAddress.sin_port = htons(m_port);
+        m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
+
+        try
+        {
+            startServer();
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "An error occurred: " << e.what() << std::endl;
+            std::cerr << "Failed to start server with PORT: " << ntohs(m_socketAddress.sin_port) << std::endl;
+            exit(0);
+        }
+    }
+    TcpServer::~TcpServer()
+    {
+        try
+        {
+            closeServer();
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "An error occurred: " << e.what() << std::endl;
+        }
         exit(0);
     }
 
-    TcpServer::startServer() {
-        try {
-            mSocket = socket(AF_INET, SOCK_STREAM, 0)
-            if (mSocket == -1) {
-                throw std::runtime_error("Cannot create socket");
-            }
-        } catch (const std::exception& e) {
-            std::cerr << "An error occurred: " << e.what() << std::endl;
-            exit(1);
+    TcpServer::startServer()
+    {
+        m_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (m_socket == -1)
+        {
+            throw std::runtime_error("Cannot create socket");
+        }
+
+        if (bind(m_socket, (sockaddr *)&m_socketAddress, sizeof(m_socketAddress)) == -1)
+        {
+            throw std::runtime_error("Cannot bind socket");
         }
     }
-    
-    TcpServer::closeServer() {
-        try {
-            if (close(mSocket) == -1) {
-                throw std::runtime_error("Cannot close socket");
-            }
-        }
-        catch (const std::exception& e) {
-            std::cerr << "An error occurred: " << e.what() << std::endl;
-            exit(1);
+
+    TcpServer::closeServer()
+    {
+        if (close(m_socket) == -1)
+        {
+            throw std::runtime_error("Cannot close socket");
         }
     }
 }
--- http_tcpServer.hpp
+++ http_tcpServer.hpp
@@ -2,16 +2,23 @@
 #define HTTP_TCPSERVER
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <string>
 
-class TcpServer {
-    public:
-        TcpServer();
-        ~TcpServer();
-    
-    private:
-        startServer();
-        closeServer();
-        int mSocket;
+class TcpServer
+{
+public:
+    TcpServer();
+    ~TcpServer();
+
+private:
+    int m_socket;
+    std::string m_ip_address;
+    unsigned int m_ip_address_len;
+    int m_port;
+    struct sockaddr_in m_socketAddress;
+
+    startServer();
+    closeServer();
 }
 
 #endif
